6db4f7779483b9d4f233a58f4e34847cc9246d52
Corrupt-77 <2292234507@qq.com>
2026-08-03 00:14:20 +0800

    SW v21 cache bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dxy-workbench-v20';
+const CACHE = 'dxy-workbench-v21';
 const ASSETS = ['./', './index.html', './daily-topics.js', './manifest.webmanifest', './icon.svg', './德馨远二维码.png', './三伏天宣传-01.jpg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})).then(() => self.skipWaiting()));
